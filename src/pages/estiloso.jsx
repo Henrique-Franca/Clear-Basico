616ba394fa618bde017a69d704e85a9d2daf8c59
Henrique França <henriqsf@gmail.com>
2022-11-09 12:18:50 -0300

    Navegação entre Componentes

--- src/pages/estiloso.jsx
+++ src/pages/estiloso.jsx
@@ -1,10 +1,13 @@
 import styles from "../styles/Estiloso.module.css"
+import Link from "next/link"
 
 export default function Estiloso(){
 
     return(
         <div className={styles.roxo}>
             <h1>Estilo usando CSS Módulos</h1>
+
+                <Link href="/"><h1>Inicio</h1></Link>
         </div>
     )
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,11 @@
-
+import Link from "next/link"
 
 export default function Inicial(){
-    const titulo = <h1>Titulo incrivel</h1>
-
-    function subtitulo() {
-        return <h2>{"muito legal".toUpperCase()}</h2>
-    }
-   
-    return (
-        <div>
-        <h1>{titulo}</h1>
-        <h2>{subtitulo()}</h2>
-        <p>Estudando Frameworks</p>
-        </div>
-
-    )
+   return (
+    <div>
+        <Link href="/estiloso">
+            <h1>Estiloso</h1>
+        </Link>
+    </div>
+   )
 }
