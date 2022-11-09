317bb2ff1dc1e7ef0fe917c8c80276eeca59d614
Henrique França <henriqsf@gmail.com>
2022-11-09 11:27:13 -0300

    CSS Global #02

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,18 @@
-export default function inicio(){
-    return(
+//import React from "react"
+
+export default function Inicial(){
+    const titulo = <h1>Titulo incrivel</h1>
+
+    function subtitulo() {
+        return <h2>{"muito legal".toUpperCase()}</h2>
+    }
+   
+    return (
         <div>
-            <h1>Eprendendo frameworks</h1>
+        <h1>{titulo}</h1>
+        <h2>{subtitulo()}</h2>
+        <p>Estudando Frameworks</p>
         </div>
+
     )
 }
