c19733baca8a6ba4050f83e6f72db8905f99cd2d
Henrique França <henriqsf@gmail.com>
2022-11-10 08:43:59 -0300

    Navegação Dinamica...

---
+++ src/pages/cliente/codigo.jsx
@@ -0,0 +1,11 @@
+import Layout from "../../components/Layout";
+
+export default function ClientePorCodigo(){
+
+    return(
+        <Layout titulo="Navegação Dinâmica">
+
+        </Layout>    
+    )
+
+}
